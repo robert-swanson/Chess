6a6dd4b70b5525257a93a9c5e88ea26b68b1340f
BuildTools <unconfigured@null.spigotmc.org>
2017-05-19 16:22:58 -0500

    Setting up the Pieces

--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -31,31 +31,31 @@ public class Pawn extends Piece
 	{
 		ArrayList<Move> moves = new ArrayList<Move>();
 
-		if(!hasMoved)
-		{
-			moves.add(new Move(pos, pos.getNewPoint(2, 0), board, true));
-			moves.add(new Move(pos, pos.getNewPoint(1, 0), board, true));
-			
-			Piece p = board.getPiece(pos.getNewPoint(1, 7));
-			if(p.color != this.color)
-				moves.add(new Move(pos, pos.getNewPoint(1, 7), board, true));
-			
-			Piece p2 = board.getPiece(pos.getNewPoint(1, 1));
-			if(p.color != this.color)
-				moves.add(new Move(pos, pos.getNewPoint(1, 1), board, true));
-		}
-		else
-		{
-			moves.add(new Move(pos, pos.getNewPoint(1, 0), board));
-
-			Piece p = board.getPiece(pos.getNewPoint(1, 7));
-			if(p.color != this.color)
-				moves.add(new Move(pos, pos.getNewPoint(1, 7), board));
-			
-			Piece p2 = board.getPiece(pos.getNewPoint(1, 1));
-			if(p.color != this.color)
-				moves.add(new Move(pos, pos.getNewPoint(1, 1), board));
-		}
+//		if(!hasMoved)
+//		{
+//			moves.add(new Move(pos, pos.getNewPoint(2, 0), board, true));
+//			moves.add(new Move(pos, pos.getNewPoint(1, 0), board, true));
+//			
+//			Piece p = board.getPiece(pos.getNewPoint(1, 7));
+//			if(p.color != this.color)
+//				moves.add(new Move(pos, pos.getNewPoint(1, 7), board, true));
+//			
+//			Piece p2 = board.getPiece(pos.getNewPoint(1, 1));
+//			if(p.color != this.color)
+//				moves.add(new Move(pos, pos.getNewPoint(1, 1), board, true));
+//		}
+//		else
+//		{
+//			moves.add(new Move(pos, pos.getNewPoint(1, 0), board));
+//
+//			Piece p = board.getPiece(pos.getNewPoint(1, 7));
+//			if(p.color != this.color)
+//				moves.add(new Move(pos, pos.getNewPoint(1, 7), board));
+//			
+//			Piece p2 = board.getPiece(pos.getNewPoint(1, 1));
+//			if(p.color != this.color)
+//				moves.add(new Move(pos, pos.getNewPoint(1, 1), board));
+//		}
 
 		return moves;
 	}
